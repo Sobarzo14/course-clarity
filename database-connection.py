83918876fb77a059f4fd07b3fcb6e550a63dea8d
Sobarzo14 <63866666+Sobarzo14@users.noreply.github.com>
2024-10-17 09:30:52 -0400

    uploaded data

--- database-connection.py
+++ database-connection.py
@@ -1,20 +1,29 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import json
 import certifi
 
 
 uri = "mongodb+srv://abelardosobarzo14:bLH9zCMExAb9EmzV@course-clarity-0.cqrri.mongodb.net/?retryWrites=true&w=majority&appName=course-clarity-0"
 
 
+with open('./data/courses.json') as f:
+    data = json.load(f)
+
 client = MongoClient(uri, tlsCAFile=certifi.where(), server_api=ServerApi('1'))
 
 try:
-    database = client.get_database("sample_mflix")
-    movies = database.get_collection("movies")
-    # Query for a movie that has the title 'Back to the Future'
-    query = { "title": "Back to the Future" }
-    movie = movies.find_one(query)
-    print(movie)
+    database = client.get_database("course_syllabi")
+    collection = database.get_collection("syllabi")
+    collection.insert_one(data)
+    
+
     client.close()
+
+
+
+
 except Exception as e:
     raise Exception("Unable to find the document due to the following error: ", e)
+
+
