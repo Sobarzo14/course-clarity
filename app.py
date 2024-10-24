29cbc1de51b58683f7682f5cfaace7859c0ce439
Sobarzo14 <63866666+Sobarzo14@users.noreply.github.com>
2024-10-24 10:16:12 -0400

    Created something

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ openai.api_key = "sk-proj-BB6hdEEPTbdSVShbe91MBGv0BiwHLU2iXFzzvn24-3O0QjpUVuUaQG
 
 
 completion = openai.ChatCompletion.create(
-  model="ft:gpt-3.5-turbo-0125:personal::AJT4aDHu",
-  messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "Hello!"}
-  ]
+    model="ft:gpt-3.5-turbo-0125:personal::AJT4aDHu",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Hello!"}
+    ]
 )
 print(completion.choices[0].message)
 
